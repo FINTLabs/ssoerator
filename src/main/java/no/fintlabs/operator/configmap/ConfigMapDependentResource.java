0cf894179e732a4f2abe00311e729ac26ccbfb3d
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-02-05 16:34:32 +0100

    We now support ConfigMap.

---
+++ src/main/java/no/fintlabs/operator/configmap/ConfigMapDependentResource.java
@@ -0,0 +1,44 @@
+package no.fintlabs.operator.configmap;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import no.fintlabs.FlaisKubernetesDependentResource;
+import no.fintlabs.FlaisWorkflow;
+import no.fintlabs.Transformer;
+import no.fintlabs.operator.SsoCrd;
+import no.fintlabs.operator.SsoSpec;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+@Component
+public class ConfigMapDependentResource extends FlaisKubernetesDependentResource<ConfigMap, SsoCrd, SsoSpec> {
+
+    private final Transformer transformer;
+
+    public ConfigMapDependentResource(FlaisWorkflow<SsoCrd, SsoSpec> workflow, KubernetesClient kubernetesClient, Transformer transformer) {
+        super(ConfigMap.class, workflow, kubernetesClient);
+        this.transformer = transformer;
+    }
+
+    @Override
+    protected ConfigMap desired(SsoCrd primary, Context<SsoCrd> context) {
+        try {
+            HashMap<String, String> labels = new HashMap<>(primary.getMetadata().getLabels());
+
+            labels.put("app.kubernetes.io/managed-by", "ssoerator");
+
+            ConfigMap configMap = getKubernetesClient()
+                    .configMaps()
+                    .load(transformer.transform(primary, "k8s/config-map.yaml"))
+                    .get();
+
+            return configMap;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
