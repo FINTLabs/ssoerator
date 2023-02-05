fa63c3e6a6b50bd23bbe91528d055a6813654000
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-02-05 19:52:32 +0100

    Ignor unknown fields

--- src/main/java/no/fintlabs/operator/traefik/middleware/AuthForwardMiddlewareSpec.java
+++ src/main/java/no/fintlabs/operator/traefik/middleware/AuthForwardMiddlewareSpec.java
@@ -1,15 +1,18 @@
 package no.fintlabs.operator.traefik.middleware;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.Collections;
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthForwardMiddlewareSpec {
     private ForwardAuth forwardAuth;
 
     @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class ForwardAuth {
         private String address;
         private List<String> authResponseHeaders = Collections.singletonList("Authorization");
