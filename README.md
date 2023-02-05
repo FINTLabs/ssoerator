57cb98cb57a4e43d06c43feea37074cdba36640a
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-02-05 16:18:07 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SSOerator
 
+Adds the following resources:
+
+- [x] Service
+- [x] Deployment
+- [x] IngressRoute
+- [x] Middleware
+- [ ] NamOAuthClientApplicationResource
+- [ ] ConfigMap
+
--- src/main/java/no/fintlabs/operator/traefik/ingress/IngressRouteCrd.java
+++ src/main/java/no/fintlabs/operator/traefik/ingress/IngressRouteCrd.java
@@ -5,7 +5,6 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Kind;
 import io.fabric8.kubernetes.model.annotation.Version;
-import no.fintlabs.operator.traefik.middleware.AuthForwardMiddlewareSpec;
 
 @Group("traefik.containo.us")
 @Version("v1alpha1")
