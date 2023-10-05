afa63b58c4d74fbc6e87417ebc2e7975624e7dc5
Hknots <Hknots99@gmail.com>
2023-10-05 09:39:57 +0200

    dont allow basePath to end with / in setter

--- src/main/java/no/fintlabs/operator/SsoSpec.java
+++ src/main/java/no/fintlabs/operator/SsoSpec.java
@@ -8,4 +8,12 @@ public class SsoSpec implements FlaisSpec {
     private String basePath = "";
     private String hostname;
     private String loggingLevel = "info";
+
+    public void setBasePath(String basePath) {
+        while (basePath.endsWith("/")) {
+            basePath = basePath.substring(0, basePath.length() - 1);
+        }
+        this.basePath = basePath;
+    }
+
 }
