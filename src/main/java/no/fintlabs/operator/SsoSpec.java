81d568d2ba4a5e5640ddd1248685a32997310101
Sondre N <118734177+sondre81@users.noreply.github.com>
2023-11-30 14:36:25 +0100

    Adding upn (#2)
    * added upn support

--- src/main/java/no/fintlabs/operator/SsoSpec.java
+++ src/main/java/no/fintlabs/operator/SsoSpec.java
@@ -3,11 +3,14 @@ package no.fintlabs.operator;
 import lombok.Data;
 import no.fintlabs.FlaisSpec;
 
+import java.util.Optional;
+
 @Data
 public class SsoSpec implements FlaisSpec {
     private String basePath = "";
     private String hostname;
     private String loggingLevel = "info";
+    private String upnClaim = "email";
 
     public void setBasePath(String basePath) {
         while (basePath.endsWith("/")) {
