d01f4e3284d3a942157e35611ac567f285cff0fb
Mattis Bratland <mattis.bratland@novari.no>
2025-01-29 14:38:41 +0100

    chore: Deploy version sha-572cb0d of SSO to Beta

--- src/main/java/no/fintlabs/Transformer.java
+++ src/main/java/no/fintlabs/Transformer.java
@@ -39,7 +39,7 @@ public class Transformer {
         context.put("basePath", crd.getSpec().getBasePath());
         context.put("oauthPath", crd.getSpec().getBasePath().endsWith("/") ? "_oauth" : "/_oauth");
         context.put("upnClaim", crd.getSpec().getUpnClaim());
-        context.put("image", "ghcr.io/fintlabs/flais-auth-forward-service:sha-6930b84");
+        context.put("image", "ghcr.io/fintlabs/flais-auth-forward-service:sha-572cb0d");
         context.put("loggingLevel", crd.getSpec().getLoggingLevel());
         context.put("issuerUri", testEnvironment
                 ? "https://idp.test.felleskomponent.no/nidp/oauth/nam"
